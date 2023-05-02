359e20dddea2a32e6c10ad51e11c04c12f6fd950
Hans <121275460+chanslouie@users.noreply.github.com>
2023-05-02 22:10:57 +0800

    Add files via upload

---
+++ 13-number-challenge/script.js
@@ -0,0 +1,16 @@
+var x = Math.floor(Math.random() * 120 + 1)
+var y = Math.floor(Math.random() * 60 + 1)
+
+var sum = x + y;
+var difference = x - y;
+var product = x * y;
+var quotient = x / y;
+var remainder = x % y;
+
+console.log(x);
+console.log(y);
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
+console.log(remainder);
